a5a5af9ac28805ce913faa21e9f064d0d7a0b935
d.bolkunov <d.bolkunov@kt-team.de>
2023-10-11 13:05:27 +0400

    ebat3

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -1,9 +1,42 @@
 <?php
 
-class LocationController
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LocationController extends AbstractController
 {
-    public function __invoke()
+    #[Route('/location/{locationId}/nearby', name: 'nearby', methods: ['GET'])]
+    public function getNearby(int $locationId): Response
     {
-        
+
+
+        $db = [
+            23 => [
+                [
+                    'name' => 'Сколково',
+                    'id' => 23,
+                    'energy' => 8,
+                ],[
+                    'name' => 'Бутово',
+                    'id' => 666
+                ],
+            ],
+            18 => [
+                [
+                    'name' => 'YHkwje',
+                    'id' => 45
+                ],[
+                    'name' => 'KHljfwlkf',
+                    'id' => 234
+                ],
+            ]
+        ];
+
+
+        return $this->json($this->locationRepository->findNearby($locationId));
     }
 }
